deb19abd375d84097ebb8f6db97e714e0596579f
Juan D. Umaña <juan.umanacaro@gmail.com>
2022-11-15 15:36:16 -0500

    Built-in dataset: epiData
    A dataset with dengue cases in Orinoquia region (2017) is preloaded as example dataset for the package

---
+++ R/dengue_orinoquia_2017-data.R
@@ -0,0 +1,23 @@
+#' Epidemiological data of Orinoquia region in Colombia (2017)
+#'
+#' Linelist dataframe of dengue cases occurred in Orinoquia region (Colombia)
+#' in 2017
+#'
+#' @docType data
+#'
+#' @usage data(dengue_orinoquia_2017)
+#'
+#' @format linelist
+#'
+#' @keywords datasets
+#'
+#' @references
+#'
+#' @source Sivigila, <http://portalsivigila.ins.gov.co/buscador>
+#'
+#' @examples
+#' data(dengue_orinoquia_2017)
+#' ages_in_years <- dengue_orinoquia_2017$EDAD_REAL
+#' 
+#' 
+"dengue_orinoquia_2017"
