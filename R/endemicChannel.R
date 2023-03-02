17b41872f1691bf7b788360ad52e5dd09397d97a
juan-umana <jd.umana10@uniandes.edu.co>
2023-03-02 17:44:37 -0500

    Update endemicChannel.R

--- R/endemicChannel.R
+++ R/endemicChannel.R
@@ -1,8 +1,8 @@
 #' Returns the endemic channel of a disease
 #' 
 #' Function that builds the endemic channel of a disease time series based on the selected method and windows of observation
-#' @param incidence An incidence or incidenceRate object with the current observations
-#' @param historic An incidence or incidenceRate object with the historic observations
+#' @param incidence_object An incidence or incidenceRate object with the current observations
+#' @param incidence_historic An incidence or incidenceRate object with the historic observations
 #' @param method = "geometric" (default) A string with the mean calculation method of preference (geometric or arithmetic)
 #' @param period_window = 0 (default) A numeric value to specify the number of previous and posterior periods to include in the calculation of the current period mean
 #' @param CI = 0.95 (default) A numeric calue to specify the confidence interval to use with the geometric method
@@ -11,7 +11,12 @@
 #' @examples
 #' endemicChannel(incidence, period_window = 3, CI = 0.95, cumu = FALSE, plot = TRUE)
 #' @export
-endemicChannel <- function(incidence, historic, method = "geometric", period_window = 0, CI = 0.95, plot = FALSE) {
+endemicChannel <- function(incidence_object, incidence_historic, method = "geometric", period_window = 0, CI = 0.95, plot = FALSE) {
+  
+  
+  
+  
+  get_mode(year(incidence_object$dates))
   
   
   if (method == "geometric"){
