a4fb2981e0005363fc9b1bfeb264ef3791eacee7
juan-umana <jd.umana10@uniandes.edu.co>
2023-03-02 17:43:59 -0500

    Create get_mode.R
    Creation of a mode calculation function

---
+++ R/get_mode.R
@@ -0,0 +1,14 @@
+#' Estimates the mode of an input vector
+#' 
+#' Function that estimates the mode of an input vector
+#' @param x A numeric or character vector
+#' @return The mode of the vector
+#' @examples
+#' get_mode(x)
+#' @export
+get_mode <- function(x) {
+  un <- unique(x)
+  tab <- tabulate(match(x, un))
+  return(un[tab == max(tab)])
+}
+
