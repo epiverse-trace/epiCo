77d1dc09aea9a002613eb7b1f65d1b30d67114d8
juan-umana <jd.umana10@uniandes.edu.co>
2023-03-28 09:25:02 -0500

    Scripts update

--- R/ageRisk.R
+++ R/ageRisk.R
@@ -1,74 +1,67 @@
 #' Returns the conditional probability mass function of being infected given an age and gender
-#' 
+#'
 #' Function that returns the probability of being infected given an age
 #' @param cases A data frame with a numeric vector with the ages of cases in years and a vector with gender
 #' @param populationPyramid A dataframe with the count of individuals
 #' @param gender = TRUE (default) A boolean to indicate the disaggregation of age risk by gender
 #' @param plot = FALSE (default) A boolean for displaying a plot
-#' 
+#'
 #' @return A dataframe with the proportion or total count of individuals
-#' @examples
-#' ageRisk(linelist, popPyramid, gender = TRUE, plot = TRUE)
 #' @export
 ageRisk <- function(cases, populationPyramid, gender = TRUE, plot = FALSE) {
-  
-  if(gender == TRUE){
-    
-    cases_F <- subset(cases, Gender == "F")
+  if (gender == TRUE) {
+    cases_F <- subset(cases, eval(parse(text = 'Gender == "F"')))
     pyramid_F <- subset(populationPyramid, Gender == "F")
-    
-    hist_F <- hist(cases_F$Age, breaks = c(0:101), right = FALSE, plot = FALSE)
-    
-    riskAge_F <- data.frame(Age = pyramid_F$Age,
-                            Prob = hist_F$counts/pyramid_F$Population,
-                            Gender = rep("F",101))
-    
+
+    hist_F <- graphics::hist(cases_F$Age, breaks = c(0:101), right = FALSE, plot = FALSE)
+
+    riskAge_F <- data.frame(
+      Age = pyramid_F$Age,
+      Prob = hist_F$counts / pyramid_F$Population,
+      Gender = rep("F", 101)
+    )
+
 
     cases_M <- subset(cases, Gender == "M")
     pyramid_M <- subset(populationPyramid, Gender == "M")
-    
-    hist_M <- hist(cases_M$Age, breaks = c(0:101), right = FALSE, plot = FALSE)
-    
-    riskAge_M <- data.frame(Age = pyramid_M$Age,
-                            Prob = hist_M$counts/pyramid_M$Population,
-                            Gender = rep("M",101))
-    
-    riskAge <- rbind(riskAge_F,riskAge_M)
-    
+
+    hist_M <- graphics::hist(cases_M$Age, breaks = c(0:101), right = FALSE, plot = FALSE)
+
+    riskAge_M <- data.frame(
+      Age = pyramid_M$Age,
+      Prob = hist_M$counts / pyramid_M$Population,
+      Gender = rep("M", 101)
+    )
+
+    riskAge <- rbind(riskAge_F, riskAge_M)
   } else {
-    
-    hist_T <- hist(cases$Age, breaks = c(0:101), right = FALSE, plot = FALSE)
-    
-    riskAge <- data.frame(Age = populationPyramid$Age,
-                          Prob = hist_T$counts/populationPyramid$Population)
+    hist_T <- graphics::hist(cases$Age, breaks = c(0:101), right = FALSE, plot = FALSE)
+
+    riskAge <- data.frame(
+      Age = populationPyramid$Age,
+      Prob = hist_T$counts / populationPyramid$Population
+    )
   }
-  
-  
-  if(plot == TRUE){
-    
-    if(gender == TRUE){
-      
-      riskAge$Prob = c(-1*riskAge_F$Prob, riskAge_M$Prob)
-      
-      riskAge_plot <- ggplot(riskAge, aes(x = Age, y = Prob, fill = Gender)) + 
-        geom_bar(data = subset(riskAge, Gender == "F"), stat = "identity") + 
-        geom_bar(data = subset(riskAge, Gender == "M"), stat = "identity") +
-        coord_flip()
-      
-      riskAge$Prob = c(riskAge_F$Prob, riskAge_M$Prob)
-      
+
+
+  if (plot == TRUE) {
+    if (gender == TRUE) {
+      riskAge$Prob <- c(-1 * riskAge_F$Prob, riskAge_M$Prob)
+
+      riskAge_plot <- ggplot2::ggplot(riskAge, ggplot2::aes(x = Age, y = Prob, fill = Gender)) +
+        ggplot2::geom_bar(data = subset(riskAge, Gender == "F"), stat = "identity") +
+        ggplot2::geom_bar(data = subset(riskAge, Gender == "M"), stat = "identity") +
+        ggplot2::coord_flip()
+
+      riskAge$Prob <- c(riskAge_F$Prob, riskAge_M$Prob)
     } else {
-      
-      riskAge_plot <- ggplot(riskAge, aes(x = Age, y = Prob)) + 
+      riskAge_plot <- ggplot(riskAge, aes(x = Age, y = Prob)) +
         geom_bar(stat = "identity")
-      
     }
-    
+
     print(riskAge_plot)
-    
   }
-  
-  
+
+
   return(riskAge)
-  
 }
--- R/ethnicity.R
+++ R/ethnicity.R
@@ -1,61 +1,57 @@
 #' Provides the sociological description of ethnicities in Colombia
-#' 
+#'
 #' Function that returns the description of consulted ethnicities
 #' @param ethniclabels A numeric vector with the codes of ethnicities to consult
 #' @param plot = FALSE (defalut) A boolean for displaying a plot
-#' 
+#'
 #' @return A printed message with the description of the ethnicities
 #' @examples
-#' ethnicity(c(1,2,3,4))
+#' ethnicity(c(1, 2, 3, 4))
 #' @export
 ethnicity <- function(ethniclabels, language = "ES", plot = FALSE) {
-
   ethniclabels <- as.data.frame(ethniclabels)
-  
+
   #### ESPAÑOL ####
   indigena_ES <- "Persona de ascendencia amerindia que comparten sentimientos de identificación con su pasado aborigen, manteniendo rasgos y valores propios de su cultura tradicional, así como formas de organización y control social propios"
-  
+
   rom_ES <- "Son comunidades que tienen una identidad étnica y cultural propia; se caracterizan por una tradición nómada, y tienen su propio idioma que es el romanés"
-  
+
   raizal_ES <- "Población ubicada en el Archipiélago de San Andrés, Providencia y Santa Catalina, con raíces culturales afroanglo-antillanas, cuyos integrantes tienen rasgos socioculturales y lingüísticos claramente diferenciados del resto de la población afrocolombiana"
-  
+
   palenquero_ES <- "Población ubicada en el municipio de San Basilio de Palenque, departamento de Bolívar, donde se habla el palenquero, lenguaje criollo"
-  
+
   afro_ES <- "Persona de ascendencia afrocolombiana que poseen una cultura propia, y tienen sus propias tradiciones y costumbre d entro de la relación campo-poblado"
-  
+
   #### ENGLISH ####
   indigena_EN <- "A person of Amerindian descent who shares feelings of identification with their aboriginal past, maintaining traits and values of their traditional culture, as well as their own forms of organization and social control"
-  
-  rom_EN <- "They are communities that have their own ethnic and cultural identity; They are characterized by a nomadic tradition, and have their own language, which is Romanesque"
-  
-  raizal_EN <- "Population located in the Archipelago of San Andrés, Providencia and Santa Catalina, with Afro-Anglo-Antillean cultural roots, whose members have clearly differentiated sociocultural and linguistic traits from the rest of the Afro-Colombian population"
-  
-  palenquero_EN <- "Population located in the municipality of San Basilio de Palenque, department of Bolívar, where palenquero is spoken, a Creole language"
-  
-  afro_EN <- "Person of Afro-Colombian descent who have their own culture, and have their own traditions and customs within the rural-populated relationship"
-  
-  #####
-  
-  
-  descriptions_ES <- c(indigena_ES,rom_ES,raizal_ES,palenquero_ES,afro_ES)
-  description_EN <- c(indigena_EN,rom_EN,raizal_EN,palenquero_EN,afro_EN)
 
-  if(plot){
-    
+  rom_EN <- "They are communities that have their own ethnic and cultural identity; They are characterized by a nomadic tradition, and have their own language, which is Romanesque"
+
+  raizal_EN <- "Population located in the Archipelago of San Andrés, Providencia and Santa Catalina, with Afro-Anglo-Antillean cultural roots, whose members have clearly differentiated sociocultural and linguistic traits from the rest of the Afro-Colombian population"
+
+  palenquero_EN <- "Population located in the municipality of San Basilio de Palenque, department of Bolívar, where palenquero is spoken, a Creole language"
+
+  afro_EN <- "Person of Afro-Colombian descent who have their own culture, and have their own traditions and customs within the rural-populated relationship"
+
+  #####
+
+
+  descriptions_ES <- c(indigena_ES, rom_ES, raizal_ES, palenquero_ES, afro_ES)
+  description_EN <- c(indigena_EN, rom_EN, raizal_EN, palenquero_EN, afro_EN)
+
+  if (plot) {
     ethnHist <- ggplot(ethniclabels, aes(ethniclabels)) +
-      geom_histogram() + theme_minimal()
-    
+      geom_histogram() +
+      theme_minimal()
+
     print(ethnHist)
-    
   }
-  
+
   labels <- order(unique(ethniclabels$ethniclabels))
-  
-  if (language == "EN"){
+
+  if (language == "EN") {
     return(description_EN[labels])
   } else {
     return(descriptions_ES[labels])
   }
-  
-  
 }
