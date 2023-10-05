ea2784c69010151dfebc3af50ee5d61b7e1885de
Juan Montenegro Torres <60274234+Juanmontenegro99@users.noreply.github.com>
2023-10-05 16:23:19 -0500

    Upload data (#39)

---
+++ R/epi_data.R
@@ -0,0 +1,8 @@
+#' Epidemiological data
+#'
+#' @title epi_data
+#' @description Epidemiological data for the Tolima department for the years
+#' 2015 to 2021
+#' @name epi_data
+#' @usage data(epi_data)
+"epi_data"
