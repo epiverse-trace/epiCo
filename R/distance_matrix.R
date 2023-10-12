6e21b0f9d1bdd1e51959bafcf479d768fa4a4be3
Juan Montenegro Torres <60274234+Juanmontenegro99@users.noreply.github.com>
2023-10-12 17:20:59 -0500

    Delete unnecessary files

--- R/distance_matrix.R
+++
@@ -1,8 +0,0 @@
-#' Distance matrix between municipalities
-#'
-#' @title distance_matrix
-#' @description Distance matrix between all Colombia's municipalities by land
-#' or river.
-#' @name distance_matrix
-#' @usage data(distance_matrix)
-"distance_matrix"
