77e5c8159387516a31d696b3442c6cb79d0c8315
Juan D. Umaña <juan.umanacaro@gmail.com>
2024-06-05 17:27:21 -0500

    Update epi_data.R

--- R/epi_data.R
+++ R/epi_data.R
@@ -2,7 +2,7 @@
 #'
 #' @title epi_data
 #' @description Epidemiological data for the Tolima department for the years
-#' 2015 to 2021
+#' 2012 to 2022
 #' @name epi_data
 #' @usage data(epi_data)
 "epi_data"
