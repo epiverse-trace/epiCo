63db05808fda8f9053b2f29fa0b142ce1f095e9e
Juan Montenegro Torres <60274234+Juanmontenegro99@users.noreply.github.com>
2024-06-20 14:49:53 -0500

    Styler changes

--- R/demographics.R
+++ R/demographics.R
@@ -19,8 +19,10 @@
 #' population_pyramid("15001", 2015, sex = TRUE, total = TRUE, plot = TRUE)
 #' @export
 population_pyramid <- function(divipola_code, year, sex = TRUE, range = 5,
-                               total = TRUE, plot = FALSE, language = c("EN",
-                                                                        "ES")) {
+                               total = TRUE, plot = FALSE, language = c(
+                                 "EN",
+                                 "ES"
+                               )) {
   stopifnot(
     "`year` must be an unique value" = (length(year) == 1),
     "`divipola_code` must be a character of a positive integer" = (
@@ -53,7 +55,7 @@ population_pyramid <- function(divipola_code, year, sex = TRUE, range = 5,
 
     female_counts <- as.numeric(pop_data_dpto[104:204])
     male_counts <- as.numeric(pop_data_dpto[3:103])
-    
+
     name <- "Colombia"
   } else if (divipola_code %in% divipola_table$COD_DPTO) {
     path_1 <- system.file("extdata", "population_projection_col_1.rds",
@@ -73,11 +75,12 @@ population_pyramid <- function(divipola_code, year, sex = TRUE, range = 5,
 
     female_counts <- as.numeric(pop_data_dpto[104:204])
     male_counts <- as.numeric(pop_data_dpto[3:103])
-    
+
     name <- dplyr::filter(
       divipola_table,
       .data$COD_DPTO == divipola_code
-    ) %>% dplyr::slice(1) %>%
+    ) %>%
+      dplyr::slice(1) %>%
       dplyr::pull(.data$NOM_DPTO)
     name <- tolower(substr(name, 1, 1)) %>%
       toupper() %>%
@@ -100,7 +103,7 @@ population_pyramid <- function(divipola_code, year, sex = TRUE, range = 5,
 
     female_counts <- as.numeric(pop_data_mun[89:174])
     male_counts <- as.numeric(pop_data_mun[3:88])
-    
+
     name <- dplyr::filter(
       divipola_table,
       .data$COD_MPIO == divipola_code
@@ -154,9 +157,10 @@ population_pyramid <- function(divipola_code, year, sex = TRUE, range = 5,
 
   if (plot) {
     pop_pyramid_plot <- population_pyramid_plot(pop_pyramid,
-                                                language = language, sex = sex)
+      language = language, sex = sex
+    )
     if (total) {
-      if (language == "EN"){
+      if (language == "EN") {
         pop_pyramid_plot <- pop_pyramid_plot +
           ggplot2::ylab("Total population") +
           ggplot2::ggtitle(paste(name, "population pyramid"))
@@ -166,7 +170,7 @@ population_pyramid <- function(divipola_code, year, sex = TRUE, range = 5,
           ggplot2::ggtitle(paste("Pirámide poblacional", name))
       }
     } else {
-      if (language == "EN"){
+      if (language == "EN") {
         pop_pyramid_plot <- pop_pyramid_plot +
           ggplot2::ylab("Proportion of population") +
           ggplot2::ggtitle(paste(name, "population pyramid"))
@@ -224,13 +228,13 @@ population_pyramid_plot <- function(pop_pyramid, language, sex = TRUE) {
         }
       ) +
       ggplot2::coord_flip()
-    if (language == "EN"){
+    if (language == "EN") {
       pop_pyramid_plot <- pop_pyramid_plot +
         ggplot2::scale_x_continuous(
           name = "Age",
           breaks = unique(pop_pyramid$age),
           labels = unique(pop_pyramid$age)
-        ) 
+        )
     } else {
       pop_pyramid_plot <- pop_pyramid_plot +
         ggplot2::scale_x_continuous(
@@ -340,8 +344,10 @@ age_risk <- function(age, population_pyramid, sex = NULL, plot = FALSE,
 
   if (plot) {
     if (!is.null(sex)) {
-      age_risk_plot <- population_pyramid_plot(age_risk, language = language,
-                                               sex = TRUE)
+      age_risk_plot <- population_pyramid_plot(age_risk,
+        language = language,
+        sex = TRUE
+      )
       if (language == "EN") {
         # nolint start
         age_risk_plot <- age_risk_plot +
@@ -356,8 +362,10 @@ age_risk <- function(age, population_pyramid, sex = NULL, plot = FALSE,
         # nolint end
       }
     } else {
-      age_risk_plot <- population_pyramid_plot(age_risk, language = language,
-                                               sex = FALSE)
+      age_risk_plot <- population_pyramid_plot(age_risk,
+        language = language,
+        sex = FALSE
+      )
       if (language == "EN") {
         age_risk_plot <- age_risk_plot +
           # nolint start
--- R/spatiotemporal.R
+++ R/spatiotemporal.R
@@ -60,7 +60,6 @@ neighborhoods <- function(query_vector, threshold = 2) {
 #' @param threshold Maximum traveling time around each municipality.
 #' @param plot if TRUE, returns a plot of influential observations in the
 #' Moran's plot.
-#' @param language Language for plot components
 
 #' @return List of Moran's I clustering analysis, giving the quadrant of each
 #' observation, influential values.
@@ -73,7 +72,7 @@ neighborhoods <- function(query_vector, threshold = 2) {
 #' }
 #' @export
 morans_index <- function(incidence_object, scale = 100000, threshold = 2,
-                         plot = TRUE, language = c("EN","ES")) {
+                         plot = TRUE) {
   stopifnot(
     "`incidence_object` must have incidence class" =
       (inherits(incidence_object, "incidence")),
@@ -83,8 +82,6 @@ morans_index <- function(incidence_object, scale = 100000, threshold = 2,
     "`threshold` must be numeric" = (is.numeric(threshold)),
     "`plot` must be boolean" = (is.logical(plot))
   )
-  language <- match.arg(language)
-  
   incidence_rate <- incidence_rate(
     incidence_object = incidence_object,
     level = 2, scale = scale
@@ -149,16 +146,6 @@ morans_index <- function(incidence_object, scale = 100000, threshold = 2,
   }
   # Plot
   if (plot) {
-    map_title <- paste0("Local Moran's Index Clusters </br>",
-                        lubridate::epiyear(incidence_object$dates),
-                        "-W",  lubridate::epiweek(incidence_object$dates),
-                        " to ", incidence_object$interval, " later" )
-    if(language == "ES"){
-      map_title <- paste0("Clústers del Índice de Moran Local </br>",
-                          lubridate::epiyear(incidence_object$dates),
-                          "-SE",  lubridate::epiweek(incidence_object$dates),
-                          " a ", incidence_object$interval, " después" )
-    }
     path_2 <- system.file("extdata", "spatial_polygons_col_2.rda",
       package = "epiCo"
     )
@@ -216,15 +203,14 @@ morans_index <- function(incidence_object, scale = 100000, threshold = 2,
         fillOpacity = 0.65
       ) %>%
       leaflet::addLegend("bottomright",
-        pal = pal,
-        values = ~ c(
+        pal = pal, values = ~ c(
           "High-High",
           "Low-Low",
           "Low-High",
           "High-Low",
           "Not Significant"
         ),
-        title = map_title,
+        title = "Local Moran's Index Clusters",
         opacity = 1
       )
     # nolint end
